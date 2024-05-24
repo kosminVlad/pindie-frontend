2bf94120f8250863e9445f8ac4655a4511cfdc6c
kosmin <ksagwey.vlad@mail.ru>
2024-05-24 14:19:42 +0700

    123

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://pindie-backend.nomoredomainswork.ru/api' 
+export const BASE_URL = 'https://pindie-backend-21.nomoredomainswork.ru' 
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
